c4a4e4614435ed1e4f31ebaaabf36336f8a08a6c
Shawn Lawson <lawsos2@rpi.edu>
2017-11-03 14:13:18 -0400

    works?

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -11,33 +11,34 @@ Bubble::Bubble() {
 	color.b = 255;
 	radius = 30;
 	// variables usedful for isRemoved
-	leftX = position.x - (radius / 2);
-	topY = position.y - (radius / 2);
-	rightX = position.x + (radius / 2);
-	bottomY = position.y + (radius / 2);
+    //only calculated once at creation time
+//	leftX = position.x - (radius / 2);
+//	topY = position.y - (radius / 2);
+//	rightX = position.x + (radius / 2);
+//	bottomY = position.y + (radius / 2);
 }
 
 //determines whether bubble stays or goes
-bool Bubble::isRemoved(ofxCvGrayscaleImage refImg, ofPixels pixels) {
-	int movementAmt = 0;
-	pixels = refImg.getPixels();
-	for (int y = topY; y < bottomY; y++) {
-		for (int x = leftX; x < rightX; x++) {
-			// changed line of code: it seems to have a problem with y* width of ref img
-			// because if it is just y + x, code runs fine
-			// if (pixels.getColor((y*refImg.getWidth())+x).getLightness() > 150) {
-			if (pixels.getColor(y+x).getLightness() > 150) {
-				movementAmt++;
-			}
-		}
-	}
-	if (movementAmt > 5) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
+//bool Bubble::isRemoved(ofxCvGrayscaleImage refImg, ofPixels pixels) {
+//	int movementAmt = 0;
+//	pixels = refImg.getPixels();
+//	for (int y = topY; y < bottomY; y++) {
+//		for (int x = leftX; x < rightX; x++) {
+//			// changed line of code: it seems to have a problem with y* width of ref img
+//			// because if it is just y + x, code runs fine
+//			// if (pixels.getColor((y*refImg.getWidth())+x).getLightness() > 150) {
+//			if (pixels.getColor(y+x).getLightness() > 150) {
+//				movementAmt++;
+//			}
+//		}
+//	}
+//	if (movementAmt > 5) {
+//		return true;
+//	}
+//	else {
+//		return false;
+//	}
+//}
 
 
 //--------------------------------------------------------------
@@ -68,9 +69,25 @@ void ofApp::update() {
 	current = colorImg;
 	difference.absDiff(current, previous);
 	difference.threshold(30);
+    difference.mirror(false, true);
+    difference.flagImageChanged();
 
 	for (int i = 0; i < bubbles.size(); ++i) {
-		if (bubbles[i].isRemoved(difference, pixels) == true) {
+        int movementAmt = 0;
+        pixels = difference.getPixels();
+        float leftX = bubbles[i].position.x - (bubbles[i].radius / 2);
+        float topY = bubbles[i].position.y - (bubbles[i].radius / 2);
+        float rightX = bubbles[i].position.x + (bubbles[i].radius / 2);
+        float bottomY = bubbles[i].position.y + (bubbles[i].radius / 2);
+        for (int y = topY; y < bottomY; y++) {
+            for (int x = leftX; x < rightX; x++) {
+                if(pixels.getColor(x, y).getLightness() > 150) {
+                    movementAmt++;
+                }
+            }
+        }
+
+		if (movementAmt > 5) {
 			bubbles.erase((bubbles.begin() + i));
 			--i; //make sure you don't skip over a bubble
 		}
@@ -109,6 +126,7 @@ void ofApp::draw() {
 			ofDrawCircle(bubbles[i].position.x, bubbles[i].position.y, bubbles[i].radius);
 		}
 	}
+//    difference.draw(0,0, 320, 240);
 }
 
 //Spacebar clears the bubbles if you feel like it
